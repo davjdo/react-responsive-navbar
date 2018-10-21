8e18bda391f11b6bab4a2cb06a7a13510c4f788a
davjdo <davido_204@hotmail.com>
2018-10-21 23:31:26 +0200

    add Backdrop component and finish SideDrawer.css component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Toolbar from './components/Toolbar/Toolbar';
 import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div style={{ height: '100%' }}>
         <Toolbar />
         <SideDrawer />
+        <Backdrop />
         <main style={{ marginTop: '64px' }}>
           <p>This is the page content</p>
         </main>
