54b16ac524936d7047402ce24b0ff168524a54a9
davjdo <davido_204@hotmail.com>
2018-10-21 22:21:31 +0200

    add DrawerToggleButton component

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 import './Toolbar.css';
 
 const Toolbar = props => {
   return (
     <header className="toolbar">
       <nav className="toolbar__navigation">
-        <div />
+        <div>
+          <DrawerToggleButton />
+        </div>
         <div className="toolbar__logo">
           <Link to="/">The Logo</Link>
         </div>
